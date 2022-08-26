6315f0e779d8886b4e1f73fa976ff8f430a87d2a
newton-climate <newt.n94@gmail.com>
2022-08-26 16:58:46 -0700

    using adaptive_inversion for vertical profile retrieval

--- vertical_profile/ch4_profile.jl
+++ vertical_profile/ch4_profile.jl
@@ -1,4 +1,4 @@
-using SpectralFits, vSmartMOM, Dates, JLD2, Revise, Statistics
+using SpectralFits, vSmartMOM, Dates, JLD2, Revise, Statistics, Plots
 include("util.jl")
 
 ## define spectral window
@@ -9,7 +9,7 @@ include("util.jl")
 #datadir = "../../retrieval/julia/data"
 datadir = "/net/fluo/data1/data/NIST/DCS_A/"
 
-## get a measurement struct 
+# get a measurement struct 
 data = read_DCS_data(joinpath(datadir, "20160921.h5"))
 data = take_time_average!(data, δt=Minute(15))
 measurement = get_measurement(10, data, ν_grid[1], ν_grid[end])
@@ -21,16 +21,17 @@ inversion_setup = Dict{String,Any}(
     "fit_temperature" => false,
 "verbose_mode" => true,
     "architecture" => CPU(),
-    "γ" => 1.0,
+    "γ" => 5.0,
 "fit_column" => true)
 
 ## get molecular data
 datadir = "/net/fluo/data1/data/NIST/spectra/"
 CH₄ = get_molecule_info("CH4", joinpath(datadir, "hit08_12CH4.jld2"), ν_grid)
 H₂O = get_molecule_info("H2O", joinpath(datadir, "tccon_H2O.jld2"), ν_grid)
+CO₂ = get_molecule_info("CO2", joinpath(datadir, "hit20_12CO2.jld2"), ν_grid)
 
 # store molecules and models in dictionary
-molecules = [H₂O, CH₄]
+molecules = [H₂O, CH₄, CO₂]
 spec = setup_molecules(molecules)
 
 ## customize the atmospheric profile
@@ -58,19 +59,22 @@ vcd = SpectralFits.calc_vcd.(measurement.pressure, measurement.temperature, 2*δ
 #vcd = ones(n)
 # true state
 x_true = StateVector("H2O" => h2o .* vcd,
-                                                         "CH4" => ch4 .* vcd,
+                     "CH4" => ch4 .* vcd,
+                     "CO2" => co2 .* vcd,
                                               "shape_parameters" => [maximum(measurement.intensity); zeros(inversion_setup["poly_degree"]-1)])
 
 #              # a priori state vector
 xₐ = StateVector("H2O" => 0.01 * vcd,
-                                                         "CH4" => 1950e-9 * vcd,
+                 "CH4" => 1950e-9 * vcd,
+                 "CO2" => 405e-6 * vcd,
                                           "shape_parameters" => [maximum(measurement.intensity); zeros(inversion_setup["poly_degree"]-1)])
 
 ## define a priori 
 a = ones(n)
-σ = OrderedDict{String, Vector{Float64}}("H2O" => 0.1*vcd,
-                                                         "CH4" => 400e-9*vcd,
-                  "shape_parameters" => 1e-4*ones(inversion_setup["poly_degree"]))
+σ = OrderedDict{String, Vector{Float64}}("H2O" => 0.07*vcd,
+                                         "CH4" => 400e-9*vcd,
+                                         "CO2" => 100e-6 * vcd,
+                  "shape_parameters" => ones(inversion_setup["poly_degree"]))
 
 ## save in setup dictionary 
 inversion_setup["σ"] = σ
@@ -78,13 +82,12 @@ inversion_setup["σ"] = σ
 ### define a customized Sₐ⁻¹
 
 # Number of gases:
-nGases = 2
+nGases = 3
 # Correlation length scale (in pressure) for the n Gases:
-pcorr = [0.01, 60.0]
-zcorr = [1.0, 5000, 5000]
+pcorr = [0.01, 50.0, 80.0]
 # Call custom make_prior_error function
-# include("custom_Sa.jl")
-#inversion_setup["Sₐ⁻¹"] = make_prior_error(σ, nGases, measurement.pressure, pcorr)
+ include("custom_Sa.jl")
+inversion_setup["Sₐ⁻¹"] = make_prior_error(σ, nGases, measurement.pressure, pcorr)
 
 
 # generate synthetic data 
@@ -98,7 +101,7 @@ measurement.σ² = σ^2
 measurement.intensity = out .+ randn(length(measurement.intensity)) * σ
 
 ## retrieval
-result = profile_inversion(f, xₐ, measurement, spec, inversion_setup)
+result = SpectralFits.adaptive_inversion(f, xₐ, measurement, spec, inversion_setup)
 
 ## save some variables for later processing 
 p0, T0 = measurement.pressure, measurement.temperature
@@ -116,3 +119,12 @@ degrees = DOF(result, ch4_ind)
 @show degrees 
 @show result.χ²
 
+p1 = plot(ch4_idealized, measurement.pressure, yflip=true,lw=2, label="retrieved", color=:red)
+plot!(1e9*ch4, measurement.pressure, yflip=true,lw=2, label="truth", color=:blue)
+title!("VMR")
+
+cK_ch4 = averaging_kernal(result, measurement.vcd, ch4_ind)
+p2 = plot(cK_ch4, measurement.pressure, yflip=true,lw=2, label=:false, color=:red)
+title!("Averaging Kernal")
+
+plot(p1, p2, layout=(1,2))
